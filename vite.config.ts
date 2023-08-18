41cbd6c614ff2a206247bc99f8850dc37e1a5b85
Yevgeniy Verchenko <verchenkoyevgeniy@gmail.com>
2023-08-18 16:31:42 +0300

    'commit'

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import autoprefixer from 'autoprefixer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://thevirtualfox.github.io/signIn',
   plugins: [react()],
   css: {
     postcss: {
